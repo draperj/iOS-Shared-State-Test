55ec70dde3682637b5688174048722a5e17cf218
Joshua Draper <joshua.draper@schroders.com>
2018-05-07 16:07:16 -0400

    Initial Commit

---
+++ SharedState/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SharedState
+//
+//  Created by Joshua Draper on 5/7/18.
+//  Copyright © 2018 Joshua Draper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
